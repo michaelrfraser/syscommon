d148d7485a8b75b719c834209d27e66bd4119518
Michael Fraser <michael.fraser@calytrix.com>
2015-03-17 09:27:33 +1100

    SC9 StringUtils::formatTime and timestamped log files
    - Added method StringUtils::formatTime()
    - The logger now right-aligns the log level marker
    - All log entries are now timestamped

--- codebase/src/cpp/syscommon/src/StringUtils.cpp
+++ codebase/src/cpp/syscommon/src/StringUtils.cpp
@@ -14,6 +14,7 @@
  */
 
 #include "syscommon/util/StringUtils.h"
+#include <time.h>
 #include <wctype.h>
 
 using namespace syscommon;
@@ -152,4 +153,19 @@ std::vector<String> StringUtils::stringSplit( const String& source, const String
 	result.push_back( lastEntry );
 
 	return result;
+}
+
+String StringUtils::formatTime( const time_t& time, const String& format )
+{
+	char buffer[2048];
+
+	// Get the local time, and string format in narrow chars
+	tm* timeLocal = ::localtime( &time );
+	std::string narrowFormat = Platform::toAnsiString( format.c_str() );
+
+	// Perform the format
+	size_t length = ::strftime( buffer, 1024, narrowFormat.c_str(), timeLocal );
+
+	// Convert back to platform string and return
+	return Platform::toPlatformString( buffer, length );
 }
