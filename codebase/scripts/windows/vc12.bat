c515cd1fb1f401e6a118fdac7fc4cfeb37164f82
Michael Fraser <michael.fraser@calytrix.com>
2016-11-24 12:00:13 +1100

    SC-23 Adds an ANSI release target with MT runtime linkage

--- codebase/scripts/windows/vc12.bat
+++ codebase/scripts/windows/vc12.bat
@@ -2,6 +2,7 @@
 call "%VS120COMNTOOLS%\vsvars32.bat"
 devenv scripts\windows\vc12\vc12.sln /Rebuild "Ansi Debug|Win32"
 devenv scripts\windows\vc12\vc12.sln /Rebuild "Ansi Release|Win32"
+devenv scripts\windows\vc12\vc12.sln /Rebuild "Ansi Release StaticRT|Win32"
 devenv scripts\windows\vc12\vc12.sln /Rebuild "Unicode Debug|Win32"
 devenv scripts\windows\vc12\vc12.sln /Rebuild "Unicode Release|Win32"
 
